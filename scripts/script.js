763379822f60194543cd9c0307b99f8bc5bdb781
Marc-Braun-72 <marc.braun72@gmx.de>
2024-07-30 09:45:15 +0200

    error-message added

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ window.onload = init;
 function init() {
     disableButtonLogin();
     showLogin('loginWindow');
+    validateLoginForm()
 }
 
 // Fehlermeldung für Login
@@ -33,4 +34,19 @@ function showLogin(elementId) {
             element.classList.add('scale-up-hor-center', 'd-flex', 'align-items-center', 'justify-content-center', 'hundertVh');
         }
     }, 1000);
+}
+
+function validateLoginForm() {
+    const emailInput = document.getElementById('login-email');
+    const emailError = document.getElementById('emailError');
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
+    emailError.style.display = 'none';
+    
+    if (!emailPattern.test(emailInput.value)) {
+        emailError.textContent = 'Bitte geben Sie eine gültige E-Mail-Adresse ein.';
+        emailError.style.display = 'block';
+        return false; 
+    }
+    return true; 
 }
