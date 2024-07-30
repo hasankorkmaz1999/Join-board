91a511ef39fff2495cbfa29b2c614054e46f7964
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-07-30 14:30:30 +0200

    edit contacts
    Co-Authored-By: Marc-Braun-72 <175475101+Marc-Braun-72@users.noreply.github.com>
    Co-Authored-By: Hasan Korkmaz <172022390+hasankorkmaz1999@users.noreply.github.com>

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,7 +1,32 @@
 let API = "https://joinapi-ad635-default-rtdb.europe-west1.firebasedatabase.app/";
-
+let userAPI = "";
+let demoAPI = "dummy-user/";
 window.onload = init;
 
-async function init() {
-    
+function init() {
+    renderData(API);
+}
+
+async function loadData(URL) {
+    try {
+        let response = await fetch(URL + ".JSON");
+        let responseToJson = await response.json();
+        console.log(responseToJson);
+        return responseToJson;
+    } catch (error) {
+        console.error("Error in loadData function:", error);
+        return null;
+    }
+}
+
+async function renderData (URL) {
+    let data = await loadData(URL);
+    /* let content = document.getElementById("ID NAME"); */
+    /* content.innerHTML = ``; */
+    if (data) {
+        for (let i = 0; i < data.users.user1ID.contact1ID.length; i++) {
+            const element = data[i];
+            console.log(element);
+        }
+    }
 }
