75bf345651c7214b7781347b526fd1e1e8cc863a
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-08-01 13:22:42 +0200

    add toast msg

--- scripts/toastMsg.js
+++ scripts/toastMsg.js
@@ -5,18 +5,27 @@
 //  dabei können wir das js überall nutzen                                    //
 ////////////////////////////////////////////////////////////////////////////////
 
+/* 
+    <div id="toast-div" class="d-none">
+        <div class="toast-text" id="toast-text"></div>
+    </div>
+*/
+
 
 function toastMessage(msg) {
-    let toastBox = document.getElementById('dummy');
-    toastBox.innerHTML = `${msg}`;
+    let toastBox = document.getElementById('toast-div');
+    let toastMsg = document.getElementById('toast-text');
+    toastMsg.innerHTML = `${msg}`;
     toastBox.classList.remove('d-none');
     toastBox.classList.add('slide-in-right');
+    toastBox.classList.add('toast');
     setTimeout(() => {
         toastBox.classList.add('slide-out-right');
-    }, 2000);
+    }, 3000);
     setTimeout(() => {
         toastBox.classList.add('d-none');
         toastBox.classList.remove('slide-out-right');
         toastBox.classList.remove('slide-in-right');
-    }, 3000);
+        toastBox.classList.remove('toast');
+    }, 3500);
 }
