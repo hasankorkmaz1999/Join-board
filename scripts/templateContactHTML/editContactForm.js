f2698dd56f255badde37145bed2d75cafeb34c0a
Marc-Braun-72 <marc.braun72@gmx.de>
2024-08-03 10:41:50 +0200

    responsive v1.6

--- scripts/templateContactHTML/editContactForm.js
+++ scripts/templateContactHTML/editContactForm.js
@@ -1,25 +1,28 @@
 function editContactForm(name, phone, email, id) {
     return /*html*/`
         <div class="contactForm">
-        <div class="contactFormLeft">
-            <img class="joinnnlogocontact" src="./icons/Capa2Edit.svg" alt="">
-            <img class="addcontacttext" src="./icons/Frame211Edit.svg" alt="">
-        </div>
-        <form class="contactFormRight" onsubmit="finishEditContact('${id}'); return false;">
-            <img src="./icons/contacticons/kontak.png" alt="">
-            <div class="contactinputfields">
-                <img onclick="closeEditOverlay()" class="closeX" src="./icons/close.svg" alt="">
-                <span id="name-error-edit" class="error-message"></span>
-                <input value="${name}" id="nameValue"  class="inputfiledsname" placeholder="Name" type="text" name="name">
-                <span id="email-error-edit" class="error-message"></span>
-                <input value="${email}" id="emailValue" class="inputfiledsemail" placeholder="Email" type="text" name="email">
-                <span id="phone-error-edit" class="error-message"></span>
-                <input value="${phone}" id="phoneValue" class="inputfiledsphone" placeholder="Phone" type="text" name="phone">
-                <div class="contactbuttons">
-                    <button type="button" onclick="closeEditOverlay()" class="cancelbutton">Cancel X</button>
-                    <button type="submit" class="createbutton">Edit contact<img src="./icons/check.svg" alt=""></button>
-                </div>
+            <div class="contactFormLeft">
+                <div class="mobile-icon-corner-right"></div>
+                <h1 class="mobile">Edit contact</h1>
+                <p class="subheader-edit-mobile">Tasks are better with a team!</p>
+                <div class="mobile-edit-underline"></div>
             </div>
-        </form>
+            <form class="contactFormRight" onsubmit="finishEditContact('${id}'); return false;">
+                <img src="./icons/contacticons/kontak.png" alt="">
+                <div class="contactinputfields">
+                    <img onclick="closeEditOverlay()" class="closeX" src="./icons/close.svg" alt="Icon close">
+                    <span id="name-error-edit" class="error-message"></span>
+                    <input value="${name}" id="nameValue"  class="inputfiledsname" placeholder="Name" type="text" name="name">
+                    <span id="email-error-edit" class="error-message"></span>
+                    <input value="${email}" id="emailValue" class="inputfiledsemail" placeholder="Email" type="text" name="email">
+                    <span id="phone-error-edit" class="error-message"></span>
+                    <input value="${phone}" id="phoneValue" class="inputfiledsphone" placeholder="Phone" type="text" name="phone">
+                    <div class="contactbuttons">
+                        <button type="button" onclick="closeEditOverlay()" class="cancelbutton">Delete</button>
+                        <button type="submit" class="saveButton">Save<img class="check-icon-mobile" src="./icons/check.svg" alt="Icon check"></button>
+                    </div>
+                </div>
+            </form>
+        </div>
     `
 }
