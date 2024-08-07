5508ac438d48ebba61ea0da243cf7199a97208c7
Marc-Braun-72 <marc.braun72@gmx.de>
2024-08-07 08:24:52 +0200

    add-task v0.6

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -13,3 +13,21 @@ function setActive(button, priority) {
     let priorityInput = document.getElementById('priority');
     priorityInput.value = priority;
 }
+
+
+function showAddTaskOverlay() {
+    let editWindow = document.getElementById('overlayEdit');
+    editWindow.classList.remove('d-none');
+    editWindow.classList.add('slide-in-right');
+}
+
+function closeAddTaskOverlay() {
+    let divID = document.getElementById('overlayEdit');
+    divID.classList.add('slide-out-right');
+    setTimeout(() => {
+        let divID = document.getElementById('overlayEdit');
+        divID.classList.add('d-none');
+        divID.classList.remove('slide-in-right');
+        divID.classList.remove('slide-out-right');
+    }, 500);
+}
