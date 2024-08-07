2cab4db00611bc3d1330630460127a437c8333eb
Marc-Braun-72 <marc.braun72@gmx.de>
2024-08-07 08:29:27 +0200

    add-task v0.6

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -16,16 +16,16 @@ function setActive(button, priority) {
 
 
 function showAddTaskOverlay() {
-    let editWindow = document.getElementById('overlayEdit');
+    let editWindow = document.getElementById('overlayAddTask');
     editWindow.classList.remove('d-none');
     editWindow.classList.add('slide-in-right');
 }
 
 function closeAddTaskOverlay() {
-    let divID = document.getElementById('overlayEdit');
+    let divID = document.getElementById('overlayAddTask');
     divID.classList.add('slide-out-right');
     setTimeout(() => {
-        let divID = document.getElementById('overlayEdit');
+        let divID = document.getElementById('overlayAddTask');
         divID.classList.add('d-none');
         divID.classList.remove('slide-in-right');
         divID.classList.remove('slide-out-right');
