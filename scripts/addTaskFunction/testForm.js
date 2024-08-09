dae7c73da7d870d6abbf907a0c974b5b82606b15
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-08-09 11:52:58 +0200

    add form val and error msg

--- scripts/addTaskFunction/testForm.js
+++ scripts/addTaskFunction/testForm.js
@@ -1,23 +1,35 @@
 function validateAndSanitizeForm() {
     return new Promise((resolve, reject) => {
         const taskTitle = document.getElementById("addTaskTitle");
+        const taskTitleError = document.getElementById("taskTitleError");
         const description = document.getElementById("description");
         const date = document.getElementById("prioDate");
         const category = document.getElementById("category");
         const subtasks = document.getElementById("subtasks");
 
-        // Überprüfen, ob die Pflichtfelder ausgefüllt sind
-        if (!taskTitle.value.trim() || !date.value.trim() || !category.value || category.value === "null") {
-            alert("Bitte füllen Sie alle erforderlichen Felder aus.");
-            return reject(new Error("Validation failed"));
+        let isValid = true;
+
+        if (!taskTitle.value.trim()) {
+            taskTitleError.classList.remove('d-non');
+            taskTitleError.classList.add('addTaskerrorMessage');
+            isValid = false;
+        } else {
+            taskTitleError.classList.remove('addTaskerrorMessage');
+            taskTitleError.classList.add('d-none');
         }
 
+        if (!date.value.trim() || !category.value || category.value === "null") {
+            alert("Bitte füllen Sie alle erforderlichen Felder aus.");
+            isValid = false;
+        }
+
+        if (!isValid) {
+            return reject(new Error("Validation failed"));
+        }
         // Symbole ersetzen, um XSS zu verhindern
         const sanitizeInput = (input) => {
             return input.replace(/[<>&"'\/\\(){}[\]=;:]/g, ' ');
         };
-
-        // Bereinigte Werte
         const sanitizedValues = {
             taskTitle: sanitizeInput(taskTitle.value),
             description: sanitizeInput(description.value),
@@ -25,8 +37,6 @@ function validateAndSanitizeForm() {
             category: category.value,
             subtasks: sanitizeInput(subtasks.value)
         };
-
-        // Validierung erfolgreich
         resolve(sanitizedValues);
     });
 }
