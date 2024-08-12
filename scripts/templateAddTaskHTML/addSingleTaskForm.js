a8b1964d1dbba04bc30bbcc6defc0b7018a0fee6
Hasan Korkmaz <hasan_k2023@outlook.com>
2024-08-12 12:01:16 +0200

    boardseite   neuster stand

--- scripts/templateAddTaskHTML/addSingleTaskForm.js
+++ scripts/templateAddTaskHTML/addSingleTaskForm.js
@@ -8,8 +8,21 @@ function addSingleTaskForm(taskData, key) {
             <p>${taskData.taskDescription}</p>
             <p>Due date: <span class="dueDate">${taskData.dueDate}</span></p>
             <p class="">Priority: <span class="taskPriority"> ${taskData.taskPriority} ${taskData.taskPriorityIcon} </span></p>
-            <p class="nurtest">Assigned To:</p>
+            <p class="AssignedTitle">Assigned To:</p>
+
+            <div class="assignedTo-container">
+                ${taskData.assignedToHTML} 
+            </div>
+
+
             <p class="Subtasks">Subtasks</p>
+
+
+            <div class="subtasks-container">
+                ${taskData.subtasksHTML} 
+            </div>
+
+
             <div class="editanddeletetask">
                 <div onclick='deleteTask("${key}")' class="deletebuttontask">Delete</div>
                 <div class="editbuttontask">Edit</div>
