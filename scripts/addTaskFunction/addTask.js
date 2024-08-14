94a707a9d2c7414805ee55f3cb4735eb74b3dd6d
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-08-14 10:57:29 +0200

    del

--- scripts/addTaskFunction/addTask.js
+++ scripts/addTaskFunction/addTask.js
@@ -130,4 +130,38 @@ async function getAssignedTo(data, content) {
     }
     addCheckboxEventListeners();
 }
+
+
+/* Auswahlbutton in addTask: Low, Medium, Urgent
+*******************************************************/
+function setActive(button, priority) {
+
+    let buttons = document.querySelectorAll('.addTaskPrioButton');
+    buttons.forEach(btn => {
+        btn.classList.remove('active-urgent', 'active-medium', 'active-low');
+    });
+
+    button.classList.add(`active-${priority}`);
+
+    let priorityInput = document.getElementById('priority');
+    priorityInput.value = priority;
+}
+
+
+function showAddTaskOverlay() {
+    let editWindow = document.getElementById('overlayAddTask');
+    editWindow.classList.remove('d-none');
+    editWindow.classList.add('slide-in-right');
+}
+
+function closeAddTaskOverlay() {
+    let divID = document.getElementById('overlayAddTask');
+    divID.classList.add('slide-out-right');
+    setTimeout(() => {
+        let divID = document.getElementById('overlayAddTask');
+        divID.classList.add('d-none');
+        divID.classList.remove('slide-in-right');
+        divID.classList.remove('slide-out-right');
+    }, 500);
+}
 // End: Initialen
