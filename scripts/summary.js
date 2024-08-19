0cb57e8f3adad86de905b5deb5aeb422106cf6c9
Metafeller <savasboas@gmail.com>
2024-08-19 22:51:33 +0200

    Update Summary
    Greeting Function and Todo / Done CSS

--- scripts/summary.js
+++ scripts/summary.js
@@ -101,24 +101,46 @@ numberTaskInProgress
 numberTaskAwaitFeedback
 */
 
-function displayGreeting() {
-    let greetingText = document.getElementById('greeting-text');
-    let currentHour = new Date().getHours();
-    let greeting;
-    try {
-        if (currentHour < 12) {
-            greeting = "Good morning,";
-        } else if (currentHour < 18) {
-            greeting = "Good afternoon,";
-        } else {
-            greeting = "Good evening,";
-        }
-    } catch (error) {
-        greeting = "Hello,";
-        console.error("Error while getting the current hour: " + error);
+// function displayGreeting() {
+//     let greetingText = document.getElementById('greeting-text');
+//     let currentHour = new Date().getHours();
+//     let greeting;
+//     try {
+//         if (currentHour < 12) {
+//             greeting = "Good morning,";
+//         } else if (currentHour < 18) {
+//             greeting = "Good afternoon,";
+//         } else {
+//             greeting = "Good evening,";
+//         }
+//     } catch (error) {
+//         greeting = "Hello,";
+//         console.error("Error while getting the current hour: " + error);
         
-    }
-    
+//     }
 
-    greetingText.innerText = greeting;
+//     greetingText.innerText = greeting;
+// }
+
+
+// JS-Function für Login und Signup Greeting Area für echten User mit ID
+function displayGreeting() {
+    let greetingContainer = document.getElementById('greeting-text');
+    let nameContainer = document.getElementById('userGreeting');  // Aktualisieren; um die richtige ID zu verwenden!
+
+    let currentHour = new Date().getHours();
+    let greetingText;
+
+    if (currentHour < 12) {
+        greetingText = 'Good morning,';
+    } else if (currentHour < 18) {
+        greetingText = 'Good afternoon,';
+    } else {
+        greetingText = 'Good evening,';
+    }
+
+    greetingContainer.textContent = greetingText;
+
+    // // Platzhalter für den Benutzernamen; "Sofia Müller" ersetzen wir später durch dynamischen Inhalt...
+    nameContainer.textContent = "EchterUserID";
 }
