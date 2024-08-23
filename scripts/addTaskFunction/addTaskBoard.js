7329a6c77bb504243c7200366afc93e833b5566c
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-08-23 11:11:27 +0200

    outsource HTML
    outsource the HTML parts to addTaskBoard

--- scripts/addTaskFunction/addTaskBoard.js
+++ scripts/addTaskFunction/addTaskBoard.js
@@ -63,56 +63,6 @@ function getQueryParams() {
 }
 
 
-function handleProgress0() {
-    let button = document.getElementById('addTaskFlexButtons');
-    button.innerHTML = `
-            <button id="createbutton" type="button" onclick="addTaskBoard(0)" class="createbutton">
-                <p class="create-mobile">Create Task</p>
-                <img class="check-icon-mobile" src="./IMGicons/check.svg" alt="Icon check">
-            </form>
-    `; 
-}
-
-
-function handleProgress1() {
-    let title = document.getElementById('addTaskH1');
-    let button = document.getElementById('addTaskFlexButtons');
-    title.innerHTML = `Create Task in To-Do`;
-    button.innerHTML = `
-            <button id="createbutton" type="button" onclick="addTaskBoard(1)" class="createbutton">
-                <p class="create-mobile">Create Task</p>
-                <img class="check-icon-mobile" src="./IMGicons/check.svg" alt="Icon check">
-            </form>
-    `; 
-}
-
-
-function handleProgress2() {
-    let title = document.getElementById('addTaskH1');
-    let button = document.getElementById('addTaskFlexButtons');
-    title.innerHTML = `Create Task in Progress`;
-    button.innerHTML = `
-            <button id="createbutton" type="button" onclick="addTaskBoard(2)" class="createbutton">
-                <p class="create-mobile">Create Task</p>
-                <img class="check-icon-mobile" src="./IMGicons/check.svg" alt="Icon check">
-            </form>
-    `; 
-}
-
-
-function handleProgress3() {
-    let title = document.getElementById('addTaskH1');
-    let button = document.getElementById('addTaskFlexButtons');
-    title.innerHTML = `Create Task Awaiting Feedback`;
-    button.innerHTML = `
-            <button id="createbutton" type="button" onclick="addTaskBoard(3)" class="createbutton">
-                <p class="create-mobile">Create Task</p>
-                <img class="check-icon-mobile" src="./IMGicons/check.svg" alt="Icon check">
-            </form>
-    `; 
-}
-
-
 function addTaskBoard(progress) {
     if (progress === 0) {
         startAddTask();
